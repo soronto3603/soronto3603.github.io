21d48d0f37a46e9eff1e72779e100c8ae4eed2b6
soronto3603 <soronto3603@datable.co.kr>
2019-12-15 22:20:34 +0900

    123

--- lotto/service/src/App.svelte
+++ lotto/service/src/App.svelte
@@ -4,6 +4,7 @@
 <script>
 	import * as d3 from 'd3';
 	import { onMount } from 'svelte';
+	// import dataset from '../data/statistics.json';
 	export let name;
 
 	onMount(async () => {
@@ -14,54 +15,48 @@
 		var n = 21;
 
 		var xScale = d3.scaleLinear()
-				.domain([0, n-1]) // input
-				.range([0, width]); // output
+				.domain([0, n-1])
+				.range([0, width]);
 
 		var yScale = d3.scaleLinear()
-				.domain([0, 1]) // input 
-				.range([height, 0]); // output 
+				.domain([0, 1])
+				.range([height, 0]);
 
-		// 7. d3's line generator
 		var line = d3.line()
-				.x(function(d, i) { return xScale(i); }) // set the x values for the line generator
-				.y(function(d) { return yScale(d.y); }) // set the y values for the line generator 
-				.curve(d3.curveMonotoneX) // apply smoothing to the line
+				.x(function(d, i) { return xScale(i); })
+				.y(function(d) { return yScale(d.y); })
+				.curve(d3.curveMonotoneX)
 
-		// 8. An array of objects of length N. Each object has key -> value pair, the key being 'y' and the value is a random number
 		var dataset = d3.range(n).map(function(d) { return {'y': d3.randomUniform(1)() } })
 
-		// 1. Add the SVG to the page and employ #2
+
 		var svg = d3.select('.line-chart').append('svg')
 				.attr('width', width + margin.left + margin.right)
 				.attr('height', height + margin.top + margin.bottom)
 			.append('g')
 				.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-		// 3. Call the x axis in a group tag
 		svg.append('g')
 				.attr('class', 'x axis')
 				.attr('transform', 'translate(0,' + height + ')')
-				.call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
+				.call(d3.axisBottom(xScale));
 
-		// 4. Call the y axis in a group tag
 		svg.append('g')
 				.attr('class', 'y axis')
-				.call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
+				.call(d3.axisLeft(yScale));
 
-		// 9. Append the path, bind the data, and call the line generator 
 		svg.append('path')
-				.datum(dataset) // 10. Binds data to the line 
-				.attr('class', 'line') // Assign a class for styling 
+				.datum(dataset)
+				.attr('class', 'line')
 				.attr('fill', 'none')
 				.attr('stroke', '#ffab00')
 				.attr('stroke-width', '3')
-				.attr('d', line); // 11. Calls the line generator 
+				.attr('d', line);
 
-		// 12. Appends a circle for each datapoint 
 		svg.selectAll('.dot')
 				.data(dataset)
-			.enter().append('circle') // Uses the enter().append() method
-				.attr('class', 'dot') // Assign a class for styling
+			.enter().append('circle')
+				.attr('class', 'dot')
 				.attr('cx', function(d, i) { return xScale(i) })
 				.attr('cy', function(d) { return yScale(d.y) })
 				.attr('r', 5)
@@ -80,24 +75,5 @@
 </main>
 
 <style>
-	.line {
-			fill: none;
-			stroke: #ffab00;
-			stroke-width: 3;
-	}
-		
-	.overlay {
-		fill: none;
-		pointer-events: all;
-	}
 
-	/* Style the dots by assigning a fill and stroke */
-	.dot {
-			
-	}
-		
-	.focus circle {
-		fill: none;
-		stroke: steelblue;
-	}
 </style>
